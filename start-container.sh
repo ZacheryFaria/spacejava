65bff7b5eb240963d311152389c87f28a2582073
Zachery Faria <zacheryfaria@gmail.com>
2026-01-01 18:59:31 -0600

    Fix startup

--- start-container.sh
+++ start-container.sh
@@ -1,3 +1,3 @@
 docker stop spacejava
 docker rm spacejava
-docker run -p 9095:8080 -d --restart always --privileged -v /home/zach/spacejava/build/libs/space-0.0.1-SNAPSHOT.jar:/app/space.jar --name spacejava space-runner 
+docker run -p 9095:8080 --env-file=.env -d --restart always --privileged -v /home/zach/spacejava/build/libs/space-0.0.1-SNAPSHOT.jar:/app/space.jar --name spacejava space-runner 
